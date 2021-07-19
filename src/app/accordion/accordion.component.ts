31844daa8e63ed2fd938c5678971279193bccc14
Vitoria-m <vitoriazzz@outlook.com>
2021-07-19 18:06:27 -0300

    plus/minus icon with ng-template

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -14,17 +14,5 @@ export class AccordionComponent {
   hide5: boolean = true
   hide6: boolean = true
 
-  spanPlusHide1: boolean = false;
-  spanPlusHide2: boolean = false;
-  spanPlusHide3: boolean = false;
-  spanPlusHide4: boolean = false;
-  spanPlusHide5: boolean = false;
-  spanPlusHide6: boolean = false;
-
-  spanMinusShow1: boolean = false
-  spanMinusShow2: boolean = false
-  spanMinusShow3: boolean = false
-  spanMinusShow4: boolean = false
-  spanMinusShow5: boolean = false
-  spanMinusShow6: boolean = false
+  showMinusIcon: boolean = false
 }
